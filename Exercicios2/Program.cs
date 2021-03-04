4c26ca38ec7373ea3f0a23438692733f80c85df0
Murilo de Souza Neves <murilogrilosn@gmail.com>
2021-03-04 13:05:27 -0300

    :rocket: first commit

---
+++ Exercicios2/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicios2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercício 1
+
+            int numero = int.Parse(Console.ReadLine());
+
+            if(numero < 0)
+            {
+                Console.WriteLine("NEGATIVO");
+            } else
+            {
+                Console.WriteLine("NAO NEGATIVO");
+            }
+
+            // Exercício 2
+             Console.WriteLine("Ex. 2");
+             numero = int.Parse(Console.ReadLine());
+
+            if(numero % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            } else
+            {
+                Console.WriteLine("IMPAR");
+            }
+
+            // Exercício 3
+
+            string[] valores = Console.ReadLine().Split(' ');
+            int val1 = int.Parse(valores[0]);
+            int val2 = int.Parse(valores[1]);
+            int maior = 0;
+            int menor = 0;
+
+            if(val1 > val2)
+            {
+                maior = val1;
+                menor = val2;
+            } else
+            {
+                maior = val2;
+                menor = val1;
+            }
+
+            if((maior % menor) == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            } else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+
+            // Exercicio 4
+            string[] valores1 = Console.ReadLine().Split(' ');
+            int val11 = int.Parse(valores1[0]);
+            int val21 = int.Parse(valores1[1]);
+
+            int tempo = 0;
+
+            if (val11 < val21)
+            {
+                tempo = val21 - val11;
+            } else if (val11 > val21)
+            {
+                tempo = (24 - val11) + val21;
+            }
+
+            if (tempo == 0)
+            {
+                tempo = 24;
+            }
+
+            Console.WriteLine($"O JOGO DUROU {tempo} HORA(S)");
+
+            // Exercicio 5
+            double[] products = { 4.00, 4.50, 5.00, 2.00, 1.50 };
+            string[] lanche = Console.ReadLine().Split(' ');
+
+            int codigo = int.Parse(lanche[0]);
+            int qtd = int.Parse(lanche[1]);
+
+            double total = products[codigo - 1] * qtd;
+
+            Console.WriteLine($"Total: R$ {total:F2}");
+
+            // Exercicio 6
+            double valor = 0;
+            valor = double.Parse(Console.ReadLine());
+            if (valor < 0.0 || valor > 100.0)
+            {
+                Console.WriteLine("Fora de intervalo");
+            }
+            else if(valor<= 25) {
+                Console.WriteLine("Intervalo [0, 25]");
+            } else if (valor <= 50)
+            {
+                Console.WriteLine("Intervalo [25, 50]");
+            }
+            else if (valor <= 75)
+            {
+                Console.WriteLine("Intervalo [50, 75]");
+            }
+            else if (valor > 75)
+            {
+                Console.WriteLine("Intervalo [75, 100]");
+            }
+
+            // Exercicio 7
+
+            string[] coords = Console.ReadLine().Split(' ');
+            float x = float.Parse(coords[0]);
+            float y = float.Parse(coords[2]);
+
+            if(x == 0.0 && y == 0.0)
+            {
+                Console.WriteLine("Origem");
+            } else if (x == 0)
+            {
+                Console.WriteLine("Eixo X");
+            }
+            else if (y == 0 )
+            {
+                Console.WriteLine("Eixo Y");
+            } else if(x > 0 && y > 0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Q3");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("Q4");
+            }
+
+            double salary = double.Parse(Console.ReadLine());
+            double imposto = 0;
+            if(salary <= 2000.00)
+            {
+                imposto = 0;
+            } else if(salary <= 3000.00)
+            {
+                imposto = (salary - 2000) * 0.08;
+            } else if(salary <= 4500)
+            {
+                imposto = (salary - 3000) * 0.18 + 80;
+            } else if (salary > 4500) {
+                imposto = (salary - 4500) * 0.28 + 270 + 80;
+            }
+
+            if(imposto == 0)
+            {
+                Console.WriteLine("Isento");
+            } else
+            {
+                Console.WriteLine("R$ " + imposto.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+        }
+
+       
+    }
+}
---
+++ ExerciciosCsharp/ExerciciosCsharp/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace ExerciciosCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercicio 1
+            Console.Write("Digite um número: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite um número: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"SOMA = {n1 + n2}");
+
+            // Exericio 2
+            Console.Write("Digite um número: ");
+            double raio = double.Parse(Console.ReadLine());
+            double area = Math.Pow(raio, 2) * 3.14159;
+            Console.WriteLine($"A={area:F4}");
+
+            // Exercicio 3
+            Console.Write("Digite um número: ");
+            int n11 = int.Parse(Console.ReadLine());
+            Console.Write("Digite um número: ");
+            int n12 = int.Parse(Console.ReadLine());
+            Console.Write("Digite um número: ");
+            int n13 = int.Parse(Console.ReadLine());
+            Console.Write("Digite um número: ");
+            int n14 = int.Parse(Console.ReadLine());
+
+            int diff = (n11 * n12 - n13 * n14);
+            Console.WriteLine($"DIFRENÇA = {diff}");
+
+            // Exercicio 4
+            Console.Write("Digite o código: ");
+            int codigo = int.Parse(Console.ReadLine());
+            Console.Write("Digite as horas: ");
+            float horas = float.Parse(Console.ReadLine());
+            Console.Write("Digite o pagamento por hora: ");
+            float salario = float.Parse(Console.ReadLine());
+            float salarioFinal = salario * horas;
+            Console.WriteLine($"NUMBER = {codigo}");
+            Console.WriteLine($"SALARY = U$ {salarioFinal:F2}");
+
+            // Exercicio 5
+            Console.Write("Digite o código da peca: ");
+            int peca = int.Parse(Console.ReadLine());
+            Console.Write("Digite o numero peca: ");
+            float numero = float.Parse(Console.ReadLine());
+            Console.Write("Digite opreco: ");
+            float preco = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o código da peca: ");
+            int peca1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite o numero peca: ");
+            float numero2 = float.Parse(Console.ReadLine());
+            Console.Write("Digite opreco: ");
+            float preco3 = float.Parse(Console.ReadLine());
+
+            float valor = (numero * preco) + (numero2 * preco3);
+            Console.WriteLine($"VALOR A PAGAR: R$ {valor:F2}");
+
+            // Exercicio 6
+            double a, b, c, triangle, circulo, trapezio, quadrado, retangulo;
+
+            string[] valores = Console.ReadLine().Split(' ');
+
+             a = double.Parse(valores[0]);
+             b = double.Parse(valores[1]);
+             c = double.Parse(valores[2]);
+
+             triangle = a * c / 2 ;
+             circulo = (c * c) * 3.14159 ;
+             trapezio = (a + b) * c / 2 ;
+             quadrado = b * b ;
+             retangulo = a * b ;
+
+            Console.WriteLine($"TRAINGULO: {triangle:F3}");
+            Console.WriteLine($"CIRCULO: {circulo:F3}");
+            Console.WriteLine($"TRAPEZIO: {trapezio:F3}");
+            Console.WriteLine($"QUADRADO: {quadrado:F3}");
+            Console.WriteLine($"RETANGULO: {retangulo:F3}");
+        }
+    }
+}
