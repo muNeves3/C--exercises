36716262745295321281cbbc6d6a489337cf39b1
Murilo de Souza Neves <murilogrilosn@gmail.com>
2021-03-05 10:50:24 -0300

    feat: uploading exercises 3 and 4

---
+++ Exercicios3/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Exercicios3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a senha");
+            int leituraSenha = int.Parse(Console.ReadLine());
+            LerSenha(leituraSenha);
+
+            string[] coords = Console.ReadLine().Split(' ');
+            int x = int.Parse(coords[0]);
+            int y = int.Parse(coords[1]);
+
+            Cartesiano(x, y);
+
+            int codigo = int.Parse(Console.ReadLine());
+            Posto(codigo);
+        }
+
+        static void LerSenha(int senha)
+        {
+            int senhaCorreta = 2002;
+
+            while (senha != senhaCorreta)
+            {
+                Console.WriteLine("Senha Invalida");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso permitido");
+        }
+        static void Cartesiano(int x, int y)
+        {
+            while (x != 0 && y != 0)
+            {
+
+
+                if (x > 0 && y > 0)
+                {
+                    Console.WriteLine("Primeiro");
+                }
+                else if (x < 0 && y > 0)
+                {
+                    Console.WriteLine("Segundo");
+                }
+                else if (x < 0 && y < 0)
+                {
+                    Console.WriteLine("Terceiro");
+                }
+                else
+                {
+                    Console.WriteLine("Quarto");
+                }
+                string[] coords = Console.ReadLine().Split(' ');
+                x = int.Parse(coords[0]);
+                y = int.Parse(coords[1]);
+            }
+        }
+        static void Posto(int codigo)
+        {
+            int contAl = 0;
+            int contGs = 0;
+            int contDs = 0;
+
+            while (codigo != 4)
+            {
+                codigo = int.Parse(Console.ReadLine());
+                switch (codigo)
+                {
+                    case 1:
+                        contAl++;
+                        break;
+                    case 2:
+                        contGs++;
+                        break;
+                    case 3:
+                        contDs++;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+            Console.WriteLine($"MUITO OBRIGADO \nAlcool: {contAl} \nGasolina: {contGs} \nDiesel {contDs}");
+
+        }
+    }
+}
---
+++ Exercicios4/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicios4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int x = int.Parse(Console.ReadLine());
+            // leituraImpares(x);
+            // leituraIntervalo(x);
+            // MediaPonderada(x);
+            // Divisao(x);
+            // Fatorial(x);
+            // Divisores(x);
+            // Multiplicadores(x);
+        }
+
+        static void leituraImpares (int x)
+        {
+            if(x < 1 || x > 100)
+            {
+                Console.WriteLine("Digite um número de 1 até 100");
+                x = int.Parse(Console.ReadLine());
+            }
+
+            for(int i = 1; i <= x; i++)
+            {
+                int impar;
+                if(x % i != 0)
+                {
+                    impar = i;
+                    Console.WriteLine(impar);
+                }
+            }
+        }
+        static void leituraIntervalo (int n)
+        {
+                int inVal = 0;
+                int outVal = 0;
+
+            for(int i = 1; i <= n; i++)
+            {
+                int valores = int.Parse(Console.ReadLine());
+                if(valores >= 10 && valores <= 20)
+                {
+                    inVal++;
+                } else
+                {
+                    outVal++;
+                }
+            }
+            Console.WriteLine($"{inVal} in \n{outVal} out");
+        }
+        static void MediaPonderada (int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+                float media = (float.Parse(valores[0]) * 2 + float.Parse(valores[1]) * 3 + float.Parse(valores[2]) * 5) / 10;
+
+                Console.WriteLine($"{media:F1}");
+            }
+        }
+        static void Divisao (int n)
+        {  
+
+            for(int i = 1; i <= n; i ++)
+            {
+                string[] pares = Console.ReadLine().Split(' ');
+                if (float.Parse(pares[1]) == 0)
+                {
+                    Console.WriteLine("divisao impossivel");
+                }
+                else
+                {
+                    Console.WriteLine($"{float.Parse(pares[0]) / float.Parse(pares[1]):F1}");
+                }
+            }
+        }
+        static void Fatorial (int n)
+        {
+            int fatorial = n;
+            if(n == 0)
+            {
+                Console.WriteLine("1");
+                return;
+            }
+
+            for(int i = 1; i <= n; i ++) 
+            {
+                fatorial *= (n - i);
+                if((n - i) == 1)
+                {
+                    Console.WriteLine(fatorial);
+                }
+            }
+        }
+        static void Divisores (int n)
+        {
+            for(int i = 1; i <= n; i ++)
+            {
+                if(n % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        static void Multiplicadores (int n)
+        {
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"{i} {Math.Pow(i, 2)} {Math.Pow(i, 3)}");
+            }
+        }
+    }
+}
